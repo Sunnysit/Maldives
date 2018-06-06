f1125926b548bf6072531619ac46fae4216e5c19
Sunny Xue <sunny.sit26@gmail.com>
2018-06-06 14:48:50 -0700

    Create basic html

--- js/newindex.js
+++ js/newindex.js
@@ -5,3 +5,22 @@ sr.reveal('.scrollReveal-wrapper', {
   duration: 1500,
   reset: true
 });
+
+
+$(window).scroll(function () {
+
+    if($(window).scrollTop()>=800)
+    {
+        $('.index-backtopbtn').slideDown();
+    }
+    else{
+        $('.index-backtopbtn').slideUp();
+    }
+
+
+});
+
+$('.index-backtopbtn').on('click',function(){
+        
+    $('html,body').animate({scrollTop:0}, 1000);
+ });
